01bfb6d0c786985a8028db88e20668f7913fc574
Erik Cui <erikcui9020@gmail.com>
2019-09-28 22:35:12 -0700

    rename tab2 to play

---
+++ src/app/play/play.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-play',
+  templateUrl: 'play.page.html',
+  styleUrls: ['play.page.scss']
+})
+export class PlayPage {
+
+  constructor() {}
+
+}
